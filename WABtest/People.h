6fc08034a65d89fb1cdd57296c0bdc1d9a6a1147
wab <18756277303@163.com>
2017-05-20 18:12:54 +0800

    添加People类

---
+++ WABtest/People.h
@@ -0,0 +1,14 @@
+//
+//  People.h
+//  WABtest
+//
+//  Created by wab on 2017/5/20.
+//  Copyright © 2017年 WAB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface People : NSObject
+
+@property (nonatomic,strong) NSString *name;
+@end
---
+++ WABtest/People.m
@@ -0,0 +1,13 @@
+//
+//  People.m
+//  WABtest
+//
+//  Created by wab on 2017/5/20.
+//  Copyright © 2017年 WAB. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+
+@end
